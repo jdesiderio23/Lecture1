9408d48d1537a0cdd9a92109d625c528e486d8c1
Jay <s333773@studenti.polito.it>
2026-04-12 19:20:12 +0200

    lezione 16/03 1:58:59

--- gestionale/gestoreOrdini.py
+++ gestionale/gestoreOrdini.py
@@ -8,7 +8,9 @@ assicurandomi che sia eseguito solo dopo gli altri.
 """
 from collections import deque, defaultdict, Counter
 
-from gestionale.vendite.ordini import Ordine
+from gestionale.core.clienti import ClienteRecord
+from gestionale.core.prodotti import ProdottoRecord
+from gestionale.vendite.ordini import Ordine, RigaOrdine
 
 
 class GestoreOrdini:
@@ -39,6 +41,7 @@ class GestoreOrdini:
         print(f"Sto processando l'ordine di {ordine.cliente}")
         print(ordine.riepilogo())
 
+
         #Aggiornare statistiche sui prodotti venduti --
         # Laptop - 10 +1
         # Mouse - 5 +2
@@ -57,6 +60,7 @@ class GestoreOrdini:
 
     def processa_tutti_ordini(self):
         """Processa tutti gli ordini attualmente presenti in coda."""
+        print("\n" + "=" * 60)
         print(f"Processando {len(self._ordini_da_processare)} ordini")
         while self._ordini_da_processare:
             self.processa_prossimo_ordine()
@@ -64,9 +68,70 @@ class GestoreOrdini:
 
     def get_statistiche_prodotti(self, top_n: int = 5):
         """Questo metodo restituisce info sui prodotti più venduti"""
+        valori = []
+        for prodotto, quantità in self._statistiche_prodotti.most_common(top_n):
+            valori.append((prodotto, quantità))
+        return valori
 
     def get_distribuzione_categorie(self):
-        pass
+        """Questo metodo restituisce info su totale fatturato per ogni categoria presente"""
+        valori = []
+        for cat in self._ordini_per_categoria.keys():
+            ordini = self._ordini_per_categoria[cat]
+            totale_Fatturato = sum([o.totale_lordo(0.22) for o in ordini])
+            valori.append((cat, totale_Fatturato))
+        return valori
 
     def stampa_riepilogo(self):
-        pass
+        """Stampa info di massima"""
+        print("\n" + "="*60)
+        print("Stato attuale del business:")
+        print(f"Ordini correttamente gestiti: {len(self._ordini_processati)}")
+        print(f"Ordini in coda: {len(self._ordini_da_processare)}")
+
+        print("Prodotti più venduti:")
+        for prod, quantità in self.get_statistiche_prodotti():
+            print(f"{prod}: {quantità}")
+
+        print(f"Fatturato per categoria:")
+        for cat, fatturato in self.get_distribuzione_categorie():
+            print(f"{cat}: {fatturato}")
+
+def test_modulo():
+    sistema = GestoreOrdini()
+
+    ordini = [
+        Ordine([
+            RigaOrdine(ProdottoRecord("Laptop",1200.0),1),
+            RigaOrdine(ProdottoRecord("Mouse", 10.0), 3)],
+            ClienteRecord("Mario Rossi", "mario@mail.it", "Gold")),
+        Ordine([
+            RigaOrdine(ProdottoRecord("Laptop",1200.0),1),
+            RigaOrdine(ProdottoRecord("Mouse", 10.0), 2),
+            RigaOrdine(ProdottoRecord("Tablet", 500.0), 1),
+            RigaOrdine(ProdottoRecord("Cuffie", 250.0), 3)],
+            ClienteRecord("Fulvio Bianchi", "bianchi@gmail.com", "Gold")),
+        Ordine([
+            RigaOrdine(ProdottoRecord("Laptop",1200.0),2),
+            RigaOrdine(ProdottoRecord("Mouse", 10.0), 2)],
+            ClienteRecord("Giuseppe Averta", "giuseppe.averta@polito.it", "Silver")),
+        Ordine([
+            RigaOrdine(ProdottoRecord("Tablet", 900.0), 1),
+            RigaOrdine(ProdottoRecord("Cuffie", 250.0), 3)],
+            ClienteRecord("Carlo Masone", "carlo@mail.it", "Gold")),
+        Ordine([
+            RigaOrdine(ProdottoRecord("Laptop", 1200.0), 1),
+            RigaOrdine(ProdottoRecord("Mouse", 10.0), 3)],
+            ClienteRecord("Francesca Pistilli", "francesca@gmail.com", "Bronze"))
+    ]
+
+    for o in ordini:
+        sistema.add_ordine(o)
+
+    sistema.processa_tutti_ordini()
+
+    sistema.stampa_riepilogo()
+
+
+if __name__ == "__main__":
+    test_modulo()
