5c7f314a2a2ec28909e3c5bd6d7346478a5f174f
Jay <s333773@studenti.polito.it>
2026-04-16 17:20:51 +0200

    lezione 18/03 21:00

---
+++ gestionale/controller.py
@@ -0,0 +1,8 @@
+
+class Controller:
+    def __init__(self):
+        self._controller = None
+    def method1(self):
+        pass
+
+
---
+++ gestionale/view.py
@@ -0,0 +1,11 @@
+
+class View:
+
+    def __init__(self):
+        pass
+
+    def carica_interfaccia(self):
+        pass
+
+    def set_controller(self, c):
+        self._controller = c
